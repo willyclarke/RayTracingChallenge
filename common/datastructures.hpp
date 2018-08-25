87d4295cb078a8c402aba5a9f6fc351d4c2b7171
Willy Clarke <willy@clarke.no>
2018-08-25 09:34:04 +0200

    ENHANCEMENT: Added building with CMake.

---
+++ common/datastructures.hpp
@@ -0,0 +1,23 @@
+/******************************************************************************
+  * Filename : datastructures.hpp
+  * Date     : 2018 Aug 25
+  * Author   : Willy Clarke (willy@clarke.no)
+  * Version  : 0.0.1
+  * Copyright: W. Clarke
+  * License  : MIT
+  * Descripti: Data structures for My Raytracing Challenge.
+  ******************************************************************************/
+#ifndef COMMON_DATASTRUCTURES_HPP
+#define COMMON_DATASTRUCTURES_HPP
+
+namespace ww
+{
+    struct tup
+    {
+        float X{};
+        float Y{};
+        float Z{};
+        float W{};
+    };
+};
+#endif
---
+++ src/raytrace/src/main.cpp
@@ -0,0 +1,19 @@
+/******************************************************************************
+ * Filename : main.cpp
+ * Date     : 2018 Aug 25
+ * Author   : Willy Clarke (willy@clarke.no)
+ * Version  : 0.0.1
+ * Copyright: W. Clarke
+ * License  : MIT
+ * Descripti: Test program for my Raytracing challenge.
+ ******************************************************************************/
+#include "../../../common/datastructures.hpp"
+
+#include <iostream>
+
+auto main() -> int
+{
+   size_t X = 0;
+   std::cout << X << std::endl;
+   return 0;
+}
