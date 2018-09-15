cd8282c4df755fceabbb17a9db1c79731e5701ca
Willy Clarke <willy@clarke.no>
2018-09-15 07:44:18 +0200

    ENHANCEMENT: Added projectile calculation to chapter 2 code.

--- src/raytrace/src/main/main.cpp
+++ src/raytrace/src/main/main.cpp
@@ -12,7 +12,7 @@
 #include <iostream>
 
 // ---
-//NOTE: The flag GTEST_ENABLED is normally set by the CMakeLists.txt file.
+// NOTE: The flag GTEST_ENABLED is normally set by the CMakeLists.txt file.
 //      Google Test is disabled in order to be able to build with the build.linux file.
 //      This functionality may be added later, but it may require Google Test installed
 //      on this PC.
@@ -21,20 +21,25 @@
 #include "featuretest.hpp"
 #include "projectile.hpp"
 #else
+#include "../../../../common/src/main/datastructures.cpp"
 // NOTE: Stubs for the tests.
 auto RunTupleTest(int argc, char *argv[]) -> int
 {
-   std::cout << argv[0];
-   return argc;
+  std::cout << argv[0];
+  return argc;
 }
 namespace rtcch1
 {
 auto RunProjectileTest(int argc, char *argv[]) -> int
 {
-   std::cout << argv[0];
-   return argc;
+  std::cout << argv[0];
+  return argc;
 }
 };  // namespace rtcch1
+namespace rtcch2
+{
+auto RunProjectileTest(){};
+};
 #endif
 
 // ---
@@ -42,31 +47,32 @@ auto RunProjectileTest(int argc, char *argv[]) -> int
 // ---
 auto main(int argc, char *argv[]) -> int
 {
-   bool DoTheTest{};
-   bool DoTheProjectile{};
+  bool DoTheTest{};
+  bool DoTheProjectile{};
 
-   if (argc > 1)
-   {
-      std::string const Argv1{argv[1]};
-      if ("-t" == Argv1)
-      {
-         std::cout << "Do the test" << std::endl;
-         DoTheTest = true;
-      }
-      else if ("-p" == Argv1)
-      {
-         DoTheProjectile = true;
-      }
-   }
+  if (argc > 1)
+  {
+    std::string const Argv1{argv[1]};
+    if ("-t" == Argv1)
+    {
+      std::cout << "Do the test" << std::endl;
+      DoTheTest = true;
+    }
+    else if ("-p" == Argv1)
+    {
+      DoTheProjectile = true;
+    }
+  }
 
-   if (DoTheTest)
-   {
-      RunTupleTest(argc, argv);
-   }
-   else if (DoTheProjectile)
-   {
-      rtcch1::RunProjectileTest(argc, argv);
-   }
+  if (DoTheTest)
+  {
+    RunTupleTest(argc, argv);
+  }
+  else if (DoTheProjectile)
+  {
+    // rtcch1::RunProjectileTest(argc, argv);
+    rtcch2::RunProjectileTest();
+  }
 
-   return 0;
+  return 0;
 }
--- src/raytrace/src/main/projectile.hpp
+++ src/raytrace/src/main/projectile.hpp
@@ -15,55 +15,121 @@
 
 #include <datastructures.hpp>
 
-namespace rtcch1
+namespace
 {
 struct projectile
 {
-   ww::tup Position{ww::Point(0.f, 0.f, 0.f)};   //<! Point.
-   ww::tup Velocity{ww::Vector(0.f, 0.f, 0.f)};  //<! Vector.
+  ww::tup Position{ww::Point(0.f, 0.f, 0.f)};   //<! Point.
+  ww::tup Velocity{ww::Vector(0.f, 0.f, 0.f)};  //<! Vector.
 };
 
 struct world
 {
-   ww::tup Gravity{ww::Vector(0.f, 0.f, 9.81f)};  //<! Vector.
-   ww::tup Wind{ww::Vector(1.0f, 0.f, 0.f)};      //<! Vector.
+  ww::tup Gravity{ww::Vector(0.f, 0.f, 9.81f)};  //<! Vector.
+  ww::tup Wind{ww::Vector(1.0f, 0.f, 0.f)};      //<! Vector.
 };
 
 projectile Tick(world const &World, projectile const &P)
 {
-   projectile Result{};
-   Result.Position = P.Position + P.Velocity;
-   Result.Velocity = P.Velocity + World.Gravity + World.Wind;
-   return (Result);
+  projectile Result{};
+  Result.Position = P.Position + P.Velocity;
+  Result.Velocity = P.Velocity + World.Gravity + World.Wind;
+  return (Result);
 }
 
+};  // end of anonymous namespace
+
+namespace rtcch1
+{
 void RunProjectileTest(int argc, char *argv[])
 {
-   ::testing::InitGoogleTest(&argc, argv);
-   int const Result = RUN_ALL_TESTS();
-   std::cout << "Testing result was " << (Result ? "Failure." : "Success.") << std::endl;
+  ::testing::InitGoogleTest(&argc, argv);
+  int const Result = RUN_ALL_TESTS();
+  std::cout << "Testing result was " << (Result ? "Failure." : "Success.") << std::endl;
 
-   // ---
-   // NOTE: Projectile starts one unit above the origin.
-   //     : Velocity is normalized to one unit per tick.
-   // ---
-   projectile P{ww::Point(0.f, 1.f, 0.f), ww::Normal(ww::Vector(1.f, 1.f, 0.f))};
-   // ---
-   // NOTE: World gravity of -0.1 unit/tick and wind is 0.01 unit/tick.
-   // ---
-   world const World{ww::Vector(0.f, -0.01f, 0.f), ww::Vector(-0.01f, 0.f, 0.f)};
-   bool Stop{};
-   int Count{};
-   while ((P.Position.Y > 0.f) && !Stop)
-   {
-       P = Tick(World, P);
-       std::cout << P.Position << std::endl;
+  // ---
+  // NOTE: Projectile starts one unit above the origin.
+  //     : Velocity is normalized to one unit per tick.
+  // ---
+  projectile P{ww::Point(0.f, 1.f, 0.f), ww::Normal(ww::Vector(1.f, 1.f, 0.f))};
+  // ---
+  // NOTE: World gravity of -0.1 unit/tick and wind is 0.01 unit/tick.
+  // ---
+  world const World{ww::Vector(0.f, -0.01f, 0.f), ww::Vector(-0.01f, 0.f, 0.f)};
+  bool Stop{};
+  int Count{};
+  while ((P.Position.Y > 0.f) && !Stop)
+  {
+    P = Tick(World, P);
+    std::cout << P.Position << std::endl;
 
-       if (Count++ > 10000)
-       {
-           Stop = true;
-       }
-   }
+    if (Count++ > 10000)
+    {
+      Stop = true;
+    }
+  }
 }
 };  // namespace rtcch1
+namespace rtcch2
+{
+void RunProjectileTest()
+{
+  ww::tup Start = ww::Point(0.f, 1.f, 0.f);
+  ww::tup Velocity = ww::Normal(ww::Vector(1., 1.8f, 0.f)) * 11.25f;
+
+  projectile P{Start, Velocity};
+
+  ww::tup Gravity = ww::Vector(0.f, -0.1f, 0.f);
+  ww::tup Wind = ww::Vector(-0.01f, 0.f, 0.f);
+
+  world World{Gravity, Wind};
+
+  ww::canvas Canvas(900, 550);
+  ww::tup Color = ww::Color(1.f, 0.f, 0.f);
+
+  bool Stop{};
+  int Count{};
+
+  // ---
+  // So we need to scale from our world to pixel.
+  // Set up pixels per meter.
+  // ---
+  float const HPixelMeter = Canvas.W / 2000.f;  // use 2000m across.
+  auto HPixel = [&](float const Pos) -> int {
+    // Pos = 0   -> Pixel = Canvas.W/2
+    // Pos = -10 -> Pixel = 0
+    // Pos = +10 -> Pixel = Canvas.W
+    float const P = HPixelMeter * Pos + Canvas.W / 2.f;
+    int const Result = std::min<int>(Canvas.W - 1, std::max<int>(0, int(P)));
+    return Result;
+  };
+
+  // The vertical axis need to be reversed.
+  float const VPixelMeter = -Canvas.H / 2000.f;  // use 2000m across.
+  auto VPixel = [&](float const Pos) -> int {
+    // Pos y = 0    -> Pixel = Canvas.H
+    // Pos y = 2000 -> Pixel = 0
+    // Pos y = 1000 -> Pixel = Canvas.H/2
+    float const P = VPixelMeter * Pos + Canvas.H;
+    int const Result = std::min<int>(Canvas.H - 1, std::max<int>(0, int(P)));
+    std::cout << "P:" << P << ". Res:" << Result << std::endl;
+    return Result;
+  };
+
+  while ((P.Position.Y > 0.f) && !Stop)
+  {
+    P = Tick(World, P);
+    std::cout << "X->" << P.Position << std::endl;
+    ww::WritePixel(Canvas, HPixel(P.Position.X), VPixel(P.Position.Y), Color);
+
+    if (Count++ > 10000)
+    {
+      Stop = true;
+    }
+  }
+  // Finally we write the file
+  ww::WriteToPPM(Canvas, "Projectile.ppm");
+}
+
+};  // namespace rtcch2
 #endif
