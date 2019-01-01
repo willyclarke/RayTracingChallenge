622d012ad4b9efee7d718973990d1b96dd9e1140
Willy Clarke <willy@clarke.no>
2019-01-01 10:00:10 +0100

    ENHANCEMENT: Chapter 8 putting it together.
    The final scene for chapter 8, use the code to put it together.

--- src/raytrace/src/main/testsmatrix.hpp
+++ src/raytrace/src/main/testsmatrix.hpp
@@ -1688,10 +1688,10 @@ TEST(Ch7MakingAScene, ShadingAnIntersection)
     ww::prepare_computation const Comps = ww::PrepareComputations(I, R);
     ww::tup const C = ww::ShadeHit(W, Comps);
 
-	// NOTE: When shading has been implemented the color changes
-	//       The old/without shading color was:
-    //EXPECT_EQ(ww::Equal(C, ww::Color(0.90498f, 0.90498f, 0.90498f)), true);
-	// NOTE: The shaded color becomes:
+    // NOTE: When shading has been implemented the color changes
+    //       The old/without shading color was:
+    // EXPECT_EQ(ww::Equal(C, ww::Color(0.90498f, 0.90498f, 0.90498f)), true);
+    // NOTE: The shaded color becomes:
     EXPECT_EQ(ww::Equal(C, ww::Color(0.1f, 0.1f, 0.1f)), true);
   }
 }
@@ -2045,6 +2045,103 @@ TEST(Ch8Shadows, TheHitShouldOffsetThePoint)
   // NOTE: The point compares to z component to half of -EPSILON to make sure that the point
   //       has been adjusted in the correct direction.
   EXPECT_EQ(Comps.Point.Z < -ww::EPSILON / 2.f, true);
+}
+
+//------------------------------------------------------------------------------
+TEST(Ch8Shadows, PuttingItTogether)
+{
+  ww::world World = ww::World();
+  World.vPtrLights.clear();
+  World.vPtrObjects.clear();
+
+  ww::shared_ptr_object PtrFloor = ww::PtrDefaultSphere();
+  ww::object &Floor = *PtrFloor;
+  Floor.Transform = ww::Scaling(10.f, 0.01f, 10.f);
+  Floor.Material.Color = ww::Color(1.f, 0.9f, 0.9f);
+  Floor.Material.Specular = 0.f;
+  World.vPtrObjects.push_back(PtrFloor);
+
+  ww::shared_ptr_object PtrLeftWall = ww::PtrDefaultSphere();
+  ww::object &LeftWall = *PtrLeftWall;
+  // NOTE: The order in which the transformations are multiplied;
+  //       The wall need to be scaled, rotated in X, rotated in Y,
+  //       and lastly translated, so the transformations are multiplied
+  //       in reverse order.
+  LeftWall.Transform = ww::Translation(0.f, 0.f, 5.f) *  //!<
+                       ww::RotateY(-M_PI_4) *            //!<
+                       ww::RotateX(M_PI_2) *             //!<
+                       ww::Scaling(10.f, 0.01f, 10.f);
+  LeftWall.Material = Floor.Material;
+  World.vPtrObjects.push_back(PtrLeftWall);
+
+  ww::shared_ptr_object PtrRightWall = ww::PtrDefaultSphere();
+  ww::object &RightWall = *PtrRightWall;
+  RightWall.Transform = ww::Translation(0.f, 0.f, 5.f) *  //!<
+                        ww::RotateY(M_PI_4) *             //!<
+                        ww::RotateX(M_PI_2) *             //!<
+                        ww::Scaling(10.f, 0.01f, 10.f);
+  RightWall.Material = Floor.Material;
+  World.vPtrObjects.push_back(PtrRightWall);
+
+  ww::shared_ptr_object PtrMiddle = ww::PtrDefaultSphere();
+  ww::object &Middle = *PtrMiddle;
+  Middle.Transform = ww::Translation(-0.5f, 1.f, 0.5f);
+  Middle.Material.Color = ww::Color(0.1f, 1.0f, 0.5f);
+  Middle.Material.Diffuse = 0.7f;
+  Middle.Material.Specular = 0.3f;
+  World.vPtrObjects.push_back(PtrMiddle);
+
+  ww::shared_ptr_object PtrMiddleHigh = ww::PtrDefaultSphere();
+  ww::object &MiddleHigh = *PtrMiddleHigh;
+  MiddleHigh.Transform = ww::Translation(0.0f, 1.75f, -2.0f) *  //!<
+                         ww::RotateZ(5.f * M_PI_2 / 4.f) *      //!<
+                         ww::RotateY(3.f * M_PI_2 / 4.f) *      //!<
+                         ww::RotateX(1.f * M_PI_2 / 4.f) *      //!<
+                         ww::Scaling(0.9f, 0.9f, 0.2f);
+  MiddleHigh.Material.Color = ww::Color(0.8f, 0.999f, 0.f);
+  MiddleHigh.Material.Diffuse = 0.99f;
+  MiddleHigh.Material.Specular = 0.7f;
+  World.vPtrObjects.push_back(PtrMiddleHigh);
+
+  ww::shared_ptr_object PtrRightHigh = ww::PtrDefaultSphere();
+  ww::object &RightHigh = *PtrRightHigh;
+  RightHigh.Transform = ww::Translation(-1.0f, 1.0f, -3.5f) *  //!<
+                        ww::RotateZ(5.f * M_PI_2 / 4.f) *      //!<
+                        ww::RotateY(3.f * M_PI_2 / 4.f) *      //!<
+                        ww::RotateX(1.f * M_PI_2 / 7.f) *      //!<
+                        ww::Scaling(0.9f, 0.4f, 0.4f);
+  RightHigh.Material.Color = ww::Color(0.1f, 0.999f, 8.f);
+  RightHigh.Material.Diffuse = 0.99f;
+  RightHigh.Material.Specular = 0.7f;
+  World.vPtrObjects.push_back(PtrRightHigh);
+
+  ww::shared_ptr_object PtrRight = ww::PtrDefaultSphere();
+  ww::object &Right = *PtrRight;
+  Right.Transform = ww::Translation(1.5f, 0.5f, -0.5f) *  //!<
+                    ww::Scaling(0.5f, 0.5f, 0.5f);
+  Right.Material.Color = ww::Color(0.5f, 1.0f, 0.1f);
+  Right.Material.Diffuse = 0.7f;
+  Right.Material.Specular = 0.3f;
+  World.vPtrObjects.push_back(PtrRight);
+
+  ww::shared_ptr_object PtrLeft = ww::PtrDefaultSphere();
+  ww::object &Left = *PtrLeft;
+  Left.Transform = ww::Translation(-1.5f, 0.33f, -0.75f) * ww::Scaling(0.33f, 0.33f, 0.33f);
+  Left.Material.Color = ww::Color(1.0f, 0.8f, 0.1f);
+  Left.Material.Diffuse = 0.7f;
+  Left.Material.Specular = 0.3f;
+  World.vPtrObjects.push_back(PtrLeft);
+
+  ww::shared_ptr_light pLight{};
+  pLight.reset(new ww::light);
+  *pLight = ww::PointLight(ww::Point(-10.f, 10.f, -10.f), ww::Color(1.f, 1.f, 1.f));
+  World.vPtrLights.push_back(pLight);
+
+  ww::camera Camera = ww::Camera(100, 50, M_PI / 3.f);
+  Camera.Transform = ww::ViewTransform(ww::Point(0.f, 1.5f, -5.f), ww::Point(0.f, 1.f, 0.f), ww::Vector(0.f, 1.f, 0.f));
+
+  ww::canvas Canvas = ww::Render(Camera, World);
+  ww::WriteToPPM(Canvas, "Ch8MakingAScene.ppm");
 }
 //------------------------------------------------------------------------------
 void RunMatrixTest(int argc, char *argv[])
