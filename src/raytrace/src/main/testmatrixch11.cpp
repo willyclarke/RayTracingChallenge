5c982711a68f3712af5655ed8650a18bcaf08b24
Willy Clarke <willyclarke@mac.com>
2022-07-07 17:34:30 +0200

    ENHANCEMENT: Add test TheReflectedColorForAReflectiveMaterial

--- src/raytrace/src/main/testmatrixch11.cpp
+++ src/raytrace/src/main/testmatrixch11.cpp
@@ -39,3 +39,43 @@ TEST(CH11ReflectionAndRefraction, TheReflectedColorForANonReflectiveMaterial)
     EXPECT_EQ(Color == ww::Color(0.f, 0.f, 0.f), true);
   }
 }
+
+//------------------------------------------------------------------------------
+// Scenario: The reflected color for a reflective material
+TEST(CH11ReflectionAndRefraction, TheReflectedColorForAReflectiveMaterial)
+{
+  ww::world W = ww::World();
+
+  // ---
+  // Add the first plane
+  // ---
+  ww::shared_ptr_plane Shape = ww::PtrDefaultPlane();
+  Shape->Material.Reflective = 0.5f;
+  Shape->Transform = ww::TranslateScaleRotate(0.f, -1.f, 0.f, 1.f, 1.f, 1.f, 0.f, 0.f, 0.f);
+  ww::WorldAddObject(W, Shape);
+
+  ww::ray const R = ww::Ray(ww::Point(0.f, 0.f, -3.f), ww::Vector(0.f, -M_SQRT2 / 2.f, M_SQRT2 / 2.f));
+  ww::intersection const I = ww::intersection{M_SQRT2, Shape};
+  ww::prepare_computation const Comps = ww::PrepareComputations(I, R);
+  ww::tup const Color = ww::ReflectedColor(W, Comps);
+  EXPECT_EQ(Color == ww::Color(0.19032f, 0.2379f, 0.14274f), true);
+
+  // ---
+  // NOTE: Write the result to file.
+  // ---
+  ww::shared_ptr_light pLight{};
+  pLight.reset(new ww::light);
+  *pLight = ww::PointLight(ww::Point(-10.f, 50.f, 0.f), ww::Color(1.f, 1.f, 1.f) * 0.2f);
+  W.vPtrLights.push_back(pLight);
+
+  float const FieldOfView = 75.f / 180.f * M_PI;
+  ww::camera Camera = ww::Camera(256, 256, FieldOfView);
+
+  ww::tup const ViewFrom = ww::Point(0.f, 1.5f, -3.5f);
+  ww::tup const ViewTo = ww::Point(0.f, 0.f, 25.f);
+  ww::tup const UpIsY = ww::Vector(0.f, 1.f, 0.f);
+  Camera.Transform = ww::ViewTransform(ViewFrom, ViewTo, UpIsY);
+
+  ww::canvas Canvas = ww::Render(Camera, W);
+  ww::WriteToPPM(Canvas, "Ch11ReflectedColorReflectiveMaterial.ppm");
+}
