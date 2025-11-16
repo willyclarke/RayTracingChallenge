7e0035de624505fda19362c0164c5429a516d5e8
Willy Clarke <willy@clarke.no>
2025-11-16 21:53:26 +0100

    BUGFIX: Add more 4x4 inverse tests

--- ZIG/raytracer-zig/src/matrix.zig
+++ ZIG/raytracer-zig/src/matrix.zig
@@ -736,3 +736,47 @@ test "Chap3 -Calculating the inverse of a matrix" {
     try std.testing.expect(true == A.isInvertible());
     try std.testing.expect(A.inverse().equals(&B));
 }
+
+test "Chap3 -Calculating the inverse of another matrix" {
+    const A = Mat4{
+        .data = .{
+            .{ 8, -5, 9, 2 },
+            .{ 7, 5, 6, 1 },
+            .{ -6, 0, 9, 6 },
+            .{ -3, 0, -9, -4 },
+        },
+    };
+    const B = Mat4{
+        .data = .{
+            .{ -0.15385, -0.15385, -0.28205, -0.53846 },
+            .{ -0.07692, 0.12308, 0.02564, 0.03077 },
+            .{ 0.35897, 0.35897, 0.43590, 0.92308 },
+            .{ -0.69231, -0.69231, -0.76923, -1.92308 },
+        },
+    };
+
+    try std.testing.expect(true == A.isInvertible());
+    try std.testing.expect(A.inverse().equals(&B));
+}
+
+test "Chap3 -Calculating the inverse of a third matrix" {
+    const A = Mat4{
+        .data = .{
+            .{ 9, 3, 0, 9 },
+            .{ -5, -2, -6, -3 },
+            .{ -4, 9, 6, 4 },
+            .{ -7, 6, 6, 2 },
+        },
+    };
+    const B = Mat4{
+        .data = .{
+            .{ -0.04074, -0.07778, 0.14444, -0.22222 },
+            .{ -0.07778, 0.03333, 0.36667, -0.33333 },
+            .{ -0.02901, -0.14630, -0.10926, 0.12963 },
+            .{ 0.17778, 0.06667, -0.26667, 0.33333 },
+        },
+    };
+
+    try std.testing.expect(true == A.isInvertible());
+    try std.testing.expect(A.inverse().equals(&B));
+}
