1489c5fa17ec15347786c27d989266938501419b
Willy Clarke <willy@clarke.no>
2026-02-25 18:32:45 +0100

    BUGFIX: Create color coded logs

--- ZIG/raytracer-zig/src/utils.zig
+++ ZIG/raytracer-zig/src/utils.zig
@@ -2,6 +2,14 @@ const std = @import("std");
 
 var g_start_time_ns: i128 = 0;
 
+/// ---
+/// If you redirect output to a file, raw escape codes are ugly. You can check if stderr is a TTY (since std.debug.print writes to stderr):
+/// ---
+fn useColor() bool {
+    const stderr = std.fs.File.stderr();
+    return std.posix.isatty(stderr.handle);
+}
+
 /// Simple debug logger with relative time, source location and message.
 /// Usage: log(@src(), "value = {}\n", .{v});
 pub fn log(src: anytype, comptime fmt: []const u8, args: anytype) void {
@@ -15,9 +23,25 @@ pub fn log(src: anytype, comptime fmt: []const u8, args: anytype) void {
     const now_ns = std.time.nanoTimestamp();
     const delta_us: i128 = @divTrunc(now_ns - g_start_time_ns, ns_per_us);
 
-    std.debug.print(
-        "[+{d:>6} µs {s}:{d}:{d} {s}] ",
-        .{ delta_us, src.file, src.line, src.column, src.fn_name },
-    );
+    if (useColor()) {
+        const green = "\x1b[32m";
+        const acolor = "\x1b[33m";
+        const cyan = "\x1b[36m";
+        const dim = "\x1b[2m";
+        const reset = "\x1b[0m";
+        std.debug.print(
+            "{s}[+{d:>6} µs{s} {s}- {s} - {s}{d}:{d}{s} ] {s}{s}{s}{s} : ",
+            .{ dim, delta_us, reset, green, src.file, acolor, src.line, src.column, dim, reset, cyan, src.fn_name, reset },
+        );
+    } else {
+        std.debug.print("[+{d:>6} µs - {s} - {d}:{d} ] {s} : ", .{
+            delta_us,
+            src.file,
+            src.line,
+            src.column,
+            src.fn_name,
+        });
+    }
+
     std.debug.print(fmt, args);
 }
